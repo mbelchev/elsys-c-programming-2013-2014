4dd2554f4d3a274b75bdbf9d65a095ba3ca734ea
mbelchev <marianbelchev@gmail.com>
2013-10-30 09:13:01 +0200

    Update matrix.c

--- 10G/Marian_Belchev_18/Homework_4/matrix.c
+++ 10G/Marian_Belchev_18/Homework_4/matrix.c
@@ -2,24 +2,25 @@
 
 int main()
 {
-    int i, j, n, sum = 0, arr[n][n];
+    int i, j, n;
     scanf("%d", &n);
-    if (n<=100){
-    	for (i=1; i<=n; i++){
-    		for (j=1; j<=n; j++){
-    			scanf("%d", &arr[i][j]);
-    		}
-   	 }
-   	for (i=1; i<=n; i++){
-    		for (j=1; j<=n; j++){
-    			if (i<j){
-    				sum=sum+arr[i][j];
-    			}
-    		}
-   	 }
-    	printf("%d", sum);
+    float arr[n][n], sum = 0;
+    if (n<=100 && n>0){
+            for (i=1; i<=n; i++){
+                    for (j=1; j<=n; j++){
+                            scanf("%f", &arr[i][j]);
+                    }
+            }
+           for (i=1; i<=n; i++){
+                    for (j=1; j<=n; j++){
+                            if (i<j){
+                                    sum=sum+arr[i][j];
+                            }
+                    }
+            }
+            printf("%f", sum);
     } else {
-    	printf ("n<=100 !");
+            printf ("error");
     }
     return 0;
 }
